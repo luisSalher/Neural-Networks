bf0943e8de152c7a0813dd649a03dfe07c5e5389
Luis Salinas <l_Salinas@MacBook-Air-de-Luis.local>
2017-10-26 17:54:57 -0500

    added new files

---
+++ fibonacci/serieFibonacci.m
@@ -0,0 +1,26 @@
+% Serie de Fibonacci
+% Salinas Hern?ndez Luis Angel
+
+n = input('Ingresa el numero de terminos a calcular en la serie de Fibonacci: ');
+f = zeros(1,n);
+q = zeros(1,n);
+num = (1:1:n);
+
+f(1) = 1;
+f(2) = 1;
+
+for i=3:1:n
+    f(i) = f(i-1) + f(i-2);
+end
+
+disp('La serie de Fibonacci es: ');
+disp(f);
+
+plot(f);
+
+
+
+
+
+
+
